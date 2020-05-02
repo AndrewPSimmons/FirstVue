89d8358e0b45bad3ce280ed570289d548816424d
AndrewPSimmons <andrewsimmons339@gmail.com>
2020-05-02 10:06:50 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="small-container">
+    <h1>Employee</h1>
+
+    <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import EmployeeTable from "@/components/EmployeeTable.vue";
 export default {
-  name: "App",
+  name: "component-name",
   components: {
-    HelloWorld
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Andrew Simmons",
+          email: "Andrewps339@gmail.com"
+        },
+        {
+          id: 2,
+          name: "Emma Simmons",
+          email: "EMS@gmail.com"
+        },
+        {
+          id: 3,
+          name: "Jon Doe",
+          email: "JDG@gmail.com"
+        }
+      ]
+    };
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: lightcyan;
+  border: 1px solid darkred;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
